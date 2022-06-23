58fe61c6bf47ea866cc362598934d116bcc8015c
Supergamerrr <83313652+Supergamerrr@users.noreply.github.com>
2022-06-24 01:44:31 +0300

    Update lolreallythiscode.sh

--- lolreallythiscode.sh
+++ lolreallythiscode.sh
@@ -171,7 +171,6 @@ echo " "
 echo "${bold}${cyan}If you can't reach terminal in the web broser"
 echo "${bold}${cyan}try do delete all files except code.txt and server.jar"
 echo "${bold}${cyan}START-COMMAND=$START-COMMAND"
-echo "${lightblue}Терминал был запущен без proot. Был запущен с bash"
 echo "${green}--------------"
 echo " "
 if [ -f $HOME/start.sh ];
@@ -192,10 +191,11 @@ else
 if [ "$OS" = "Default" ] || [ "$OS" = "Def" ] || [ "$OS" = "default" ];
 then
       echo "${lightblue}OS not selected Using: ${yellow}Default container"
-      cd $HOME && nohup gotty -a 0.0.0.0 -p $PORT -w -c "$SHELL_USERNAME:$SHELL_PASSWORD" bash
+      cd $HOME && nohup gotty -a 0.0.0.0 -p $PORT -w -c "$SHELL_USERNAME:$SHELL_PASSWORD" bash #Default
+else
       echo "${lightblue}Selected OS: ${yellow}$OS"
       proot -S . supervisord -n &
-      cd $HOME && nohup gotty -a 0.0.0.0 -p $PORT -w -c "$SHELL_USERNAME:$SHELL_PASSWORD" ssh
+      cd $HOME && nohup gotty -a 0.0.0.0 -p $PORT -w -c "$SHELL_USERNAME:$SHELL_PASSWORD" #Debian
 fi
 fi
 curl -sSLo $HOME/bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
